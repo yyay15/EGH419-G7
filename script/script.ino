f189d44bb9daa99de8f4ba617d156cdcfdc43422
Vanessa <vanessa.li0810@gmail.com>
2021-04-07 21:23:14 +1000

    Integration Testing

--- script/script.ino
+++ script/script.ino
@@ -1,24 +1,33 @@
 /** Include files **/
-
+#include "sdCard.h"
+#include "Playback.h"
 /** Variable definitions **/
 
 /** Instantiate classes **/
-
-
+SdCard SD; 
+Speaker speaker;
 void setup(){
 // SD card set up
     // read in CSV file 
+    SD.processCSV();
 
 // NFC set up
 
 // Speaker set up
-
+    speaker.GeneralSetup();
+    speaker.WAVSetup();
 // Mic set up
+
+// testing
+String audioFile = SD.NFCtoAudio("3NFC");
+speaker.WAVSelectLoop(audioFile);
 
 }
 
 
 void loop() {
+
+    speaker.WAVLoop();
     // if (nfc code true) {
     //     NFC_code = getNFC?? 
     //     if (button pressed) {
