9e8d705e74b0832e925f02931d9bee16cc20d514
yyay15 <56522754+yyay15@users.noreply.github.com>
2021-04-26 14:27:50 +1000

    nfcClassFile

---
+++ script/nfcModule.h
@@ -0,0 +1,52 @@
+/* Lenny the Learning Buddy
+ * nfcModule.h
+ * EGH419 - G7 2021
+ * Yu Yi Alan Yu
+ * 31/03/2020
+ */
+
+#ifndef nfcModule
+#define nfcModule
+
+
+#include <DFRobot_PN532.h>
+
+#define  BLOCK_SIZE      16
+#define  PN532_IRQ      (2)
+#define  INTERRUPT      (1)
+#define  POLLING        (0)
+
+
+
+class nfcReader {
+  public:
+    DFRobot_PN532_IIC *nfcObject;
+    struct card nfcScannedTag;
+
+    nfcReader(){
+      nfcObject = new DFRobot_PN532_IIC(PN532_IRQ, POLLING);
+      // NfcAdapter nfcObject = NfcAdapter(pn532_i2c);
+    }
+
+    ~nfcReader() {
+      delete nfcObject;
+    }
+
+    void startNFC(){
+      nfcObject->begin();
+    }
+
+    boolean checkTag(){
+      return (nfcObject->scan()); 
+    }
+
+  /* Read Tag function
+  */
+  String returnUID(){
+    return (nfcObject->readUid()); 
+  }
+
+
+};
+
+#endif
